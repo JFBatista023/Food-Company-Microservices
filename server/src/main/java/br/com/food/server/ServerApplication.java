070bb18075730622f9d5afb57fc020df52473317
João Filipe <joaofilipeb045@gmail.com>
2023-11-22 10:05:48 -0300

    Enable eureka server

--- server/src/main/java/br/com/food/server/ServerApplication.java
+++ server/src/main/java/br/com/food/server/ServerApplication.java
@@ -1,9 +1,11 @@
 package br.com.food.server;
 
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@EnableEurekaServer
 public class ServerApplication {
 
 	public static void main(String[] args) {
