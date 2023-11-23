df291573e0398e768cf8068ac2a8b2d970d9b23b
João Filipe <joaofilipeb045@gmail.com>
2023-11-23 07:30:25 -0300

    Rename folder

---
+++ orders-microservice/src/main/java/br/com/food/orders/domain/entity/Order.java
@@ -0,0 +1,45 @@
+package br.com.food.orders.domain.entity;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import br.com.food.orders.domain.enums.Status;
+import jakarta.persistence.CascadeType;
+import jakarta.persistence.Entity;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.OneToMany;
+import jakarta.persistence.Table;
+import jakarta.validation.constraints.NotNull;
+
+import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.List;
+
+@Entity
+@Table(name = "orders")
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class Order {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @NotNull
+    private LocalDateTime dateTime;
+
+    @NotNull
+    @Enumerated(EnumType.STRING)
+    private Status status;
+
+    @OneToMany(cascade = CascadeType.PERSIST, mappedBy = "order")
+    private List<OrderItem> items = new ArrayList<>();
+}
---
+++ orders-microservice/src/main/java/br/com/food/orders/domain/enums/Status.java
@@ -0,0 +1,13 @@
+package br.com.food.orders.domain.enums;
+
+public enum Status {
+
+    COMPLETED,
+    CANCELED,
+    PAID,
+    NOT_AUTHORIZED,
+    CONFIRMED,
+    READY,
+    OUT_FOR_DELIVERY,
+    DELIVERED;
+}
