58824346d6fb9b54f1c5d172d08b990fa69eb1b5
João Filipe <filipeb@sefaz.pi.gov.br>
2024-04-02 09:32:54 -0300

    Iniciando migração para o RabbitMQ

---
+++ payments-microservice/src/main/java/br/com/food/payments/amqp/PaymentAMQPConfiguration.java
@@ -0,0 +1,29 @@
+package br.com.food.payments.amqp;
+
+import org.springframework.amqp.core.Queue;
+import org.springframework.amqp.rabbit.connection.ConnectionFactory;
+import org.springframework.amqp.rabbit.core.RabbitAdmin;
+import org.springframework.boot.context.event.ApplicationReadyEvent;
+import org.springframework.context.ApplicationListener;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+
+@Configuration
+public class PaymentAMQPConfiguration {
+
+    @Bean
+    public Queue createQueue() {
+        return new Queue("payment.confirmed", false);
+    }
+
+    @Bean
+    public RabbitAdmin createRabbitAdmin(ConnectionFactory conn) {
+        return new RabbitAdmin(conn);
+    }
+
+    @Bean
+    public ApplicationListener<ApplicationReadyEvent> initAdmin(RabbitAdmin rabbitAdmin) {
+        return event -> rabbitAdmin.initialize();
+    }
+}
